4efe76077e922a2ef094830efa0865f85fedf82b
Ak <akmoorthi002.com>
2023-07-23 17:28:45 +0530

    testing

--- Books/pagination.py
+++ Books/pagination.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 
 
 DEFAULT_PAGE = 1
-DEFAULT_PAGE_SIZE = 1
+DEFAULT_PAGE_SIZE = 2
 
 class CustomPagination(PageNumberPagination):
     page = DEFAULT_PAGE
